5b80b9b1f3bdc1fc23e7184eaabfb3e27e3f16a4
reez89 <nicola.rizzi89@hotmail.it>
2021-03-13 21:10:31 +0100

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   // AGGIUNGENDO QUESTI VARIABILI, RENDO DISPONIBILI ALL'INTERNO DELL'HTML , GLI ELEMENTI PRESENTI NEL FILE DB-DATA
 
   courses = COURSES; // dichiaro il mio array di oggetti dal quale pescare i miei dati.
+  
+  startDate = new Date(2000, 0, 1); 
+
+  title = COURSES[0].description;
+
+  price = 9.99;
+
+  course = COURSES[0];
 
   onCourseSelected(course:Course){
 
